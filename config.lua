ade6d28a3bdfd9d5455003ba5be5193ce39aefbb
alexius544 <alex.h.fivem1@gmail.com>
2023-02-27 22:37:18 +0100

    updated config.lua value structering

--- config.lua
+++ config.lua
@@ -17,15 +17,15 @@ config = {
     respawnColor = "~r~", -- Color used for respawn button
     chatColor = "~b~", -- The chat color overall
 
+    -- Set Armour Function
+    respawnSetting = 3, -- Set armour here: 1 = Revive ONLY, 2 = Respawn ONLY, 3 = Both Respawn & Revive (default)
+    setArmour = true, -- Set to true if you want to set the player's armour to 100 when they respawn, false if not
+    armourAmount = 100 -- Change the amount of armour to give the player when they respawn. If the above is false, this value is nullified
+
     -- Discord Perms
     usePerms = true, -- Set to true if you want to use the discord perms, false if not
     bypassRoles = { -- These roles will be exempt from the revive & respawn timer
         { "roleID", "roleName" }, -- Add as many roles as you want here, using the format below
         { "roleID", "roleName" }
     },
-
-    -- Set Armour Function
-    respawnSetting = 3, -- Set armour here: 1 = Revive ONLY, 2 = Respawn ONLY, 3 = Both Respawn & Revive (default)
-    setArmour = true, -- Set to true if you want to set the player's armour to 100 when they respawn, false if not
-    armourAmount = 100 -- Change the amount of armour to give the player when they respawn. If the above is false, this value is nullified
 }
